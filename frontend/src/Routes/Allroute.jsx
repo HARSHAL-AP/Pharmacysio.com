f45b122a9099155461b812bcab9c21e93d747f2e
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-01-28 23:52:36 +0530

    Navbar Completed With home Page Slider Designs

--- frontend/src/Routes/Allroute.jsx
+++ frontend/src/Routes/Allroute.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
-
-
+import { Route, Routes } from 'react-router-dom'
+import {Home} from "../Pages/Homepage/Home"
+import {Login} from "../Pages/Login/Login"
+import {Product} from "../Pages/Productpages/Product"
+import {SIgnup} from "../Pages/Signup/SIgnup"
+import {Wishlist} from "../Pages/wishlist/Wishlist"
+import {Cart} from "../Pages/cart/Cart"
 export const Allroute = () => {
   return (
-    <div>Allroute</div>
+    <>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+
+    <Route path="/signup" element={<SIgnup/>}/>
+    <Route path="/wishlist" element={<Wishlist/>}/>
+    <Route path="/product" element={<Product/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/cart" element={<Cart/>}/>
+
+    </Routes>
+    </>
+    
   )
 }
