30592a67e52a2835d0d7d61239bc72b1a7daadec
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-02-01 23:56:48 +0530

    Footer , Main Product Page , ANd other task completed

--- frontend/src/Components/Fotter/Fotter.jsx
+++ frontend/src/Components/Fotter/Fotter.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import "./Fotter.css"
+import logom from "../../Images/logom.jpg"
 
 export const Fotter = () => {
   return (
-    <div>Fotter</div>
+    <>
+     <div className='foter'>
+      <div>Medicine</div>
+      <div>Healthcare</div>
+      <div>Lab Tests</div>
+      <div>Fruits & Vegitables</div>
+      <div><img src={logom} alt="" className='logomfoter'/></div>
+     </div>
+     <div className='subfoter'>
+     Copyright© 2023. All Rights Reserved.
+     </div>
+    </>
+   
   )
 }
