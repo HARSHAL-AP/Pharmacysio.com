0b4a9349249547ba4d0b394e40b158d69d38958f
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-02-04 09:42:08 +0530

    Made updates on Laptest Page

--- frontend/src/Pages/Productpages/LabTest/Labtest.jsx
+++ frontend/src/Pages/Productpages/LabTest/Labtest.jsx
@@ -218,7 +218,7 @@ export const Labtest = () => {
     {data.map((el, index) => {
             return <LabCard key={index} elem={el} />;
           })}
-      <LabCard/>
+      
     </div>
     <Fotter/>
     </>
