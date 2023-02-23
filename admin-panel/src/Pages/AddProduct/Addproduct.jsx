e7d726172e5d133ada57fc7d8bf966a5d50b9876
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-02-23 23:01:15 +0530

    admin Profilr Page Updated

--- admin-panel/src/Pages/AddProduct/Addproduct.jsx
+++ admin-panel/src/Pages/AddProduct/Addproduct.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import { Navbar } from '../../Components/Navbar/Navbar'
+import React from "react";
+import { Navbar } from "../../Components/Navbar/Navbar";
+import "./addproduct.css";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Select,
+  Button
+} from "@chakra-ui/react";
+
 export const Addproduct = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <div className="pageinfo">
+        <h1> Add New Product</h1>
+      </div>
+
+      <div className="addpbody">
+        <div className="formcontener">
+          <FormLabel mt={5}>Product Title</FormLabel>
+          <Input type="text" />
+
+          <FormLabel mt={5}>Product Image</FormLabel>
+          <Input type="url" />
+
+          <FormLabel mt={5}>Product Image File</FormLabel>
+          <input type="file" />
+
+          <FormLabel mt={5}>Product price</FormLabel>
+          <Input type="number" />
+
+          <FormLabel mt={5}>Product Discount</FormLabel>
+          <Input type="number" />
+
+          <FormLabel mt={5}>Product SalePrice </FormLabel>
+          <Input type="number" />
+
+          <FormLabel mt={5}>Category</FormLabel>
+          <Select placeholder="Select option">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          <FormLabel mt={5}>Sub-Category</FormLabel>
+          <Input type="text" />
+          <Button bg="Green" color="white" mt={5}>Submit</Button>
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
